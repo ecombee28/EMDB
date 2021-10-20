e06e19a6ffcf0c0a9f895f2f4bd5663ca646c4ef
Eric Combee <45884566+ecombee28@users.noreply.github.com>
2021-10-19 22:01:05 -0700

    fixed bio and hero img

--- components/Cast.js
+++ components/Cast.js
@@ -7,8 +7,6 @@ const Cast = ({ castMember }) => {
   const castImage = ImagePaths.w500 + castMember.profile_path;
   const blankImage = "/blank-profile-picture.png";
 
-  console.log(castMember);
-
   return (
     <Link href="/person/[id]" as={`/person/${castMember.id}`}>
       <div className={style.cast_node_wrapper}>
